71ce0ced29e78c7402a5b2d9ae55cfd1b98c031f
Bluette1 <marylene.sawyer@gmail.com>
2020-09-13 19:14:48 +0200

    Add sign_in functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: %i[new create show index]
+  get 'sign_up' => 'users#new'
+  get 'sign_in'  => 'sessions#new'
+  delete 'sign_out' => 'sessions#destroy'
+  post 'sign_in' => 'sessions#create'
 end
