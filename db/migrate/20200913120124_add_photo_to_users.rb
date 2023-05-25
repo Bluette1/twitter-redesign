9997e9efd30af0684f11c3c8ef99002ecf07c34a
Bluette1 <marylene.sawyer@gmail.com>
2023-05-25 04:00:14 +0200

    Redeploy app on Fly.io
    issue #13

--- db/migrate/20200913120124_add_photo_to_users.rb
+++ db/migrate/20200913120124_add_photo_to_users.rb
@@ -1,5 +1,9 @@
 class AddPhotoToUsers < ActiveRecord::Migration[6.0]
-  def change
+  def self.up
     add_attachment :users, :photo
+  end
+
+  def self.down
+    remove_attachment :users, :photo
   end
 end
