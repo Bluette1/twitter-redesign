4025e40f706d8aa2938ea5cbf3d32c90676224f0
Bluette1 <marylene.sawyer@gmail.com>
2023-12-01 11:58:19 +0200

    Finish Lint
    Issue #1

--- app/helpers/trends_worker.rb
+++ app/helpers/trends_worker.rb
@@ -3,9 +3,9 @@ require_relative './feeder'
 class TrendsWorker
   include Sidekiq::Worker
 
-  def perform(*args)
+  def perform(*_args)
     trends = Feeder.new.send_feed
-    p "Fetching trends--------------------"
+    p 'Fetching trends--------------------'
     $redis.set('trends', trends.to_json)
     $redis.expire('trends', 2.hour.to_i)
   end
